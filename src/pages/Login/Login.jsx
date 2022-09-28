669f5629cdea2646004bf3300223a257554e27c3
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-27 23:22:35 -0300

    create event preventDefault

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import './Login.scss';
 
 const Login = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('submit');
+    }
     return(
         <div id="login">
             <h1 className="title">Login do Sistema</h1>
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
                <div className="field">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="email" />
                </div>
                <div className="field">
                     <label htmlFor="password">Senha</label>
-                    <input type="email" name="email" id="email" />
+                    <input type="password" name="password" id="password" />
                </div>
                <div className="actions">
                 <button type="submit">ENTRAR</button>
