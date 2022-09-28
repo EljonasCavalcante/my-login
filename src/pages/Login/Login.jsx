8f4a93af3f2c4e98a0fbc4b8a46acc2e88da5992
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-27 23:35:56 -0300

    functional login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import './Login.scss';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('submit');
+        console.log('submit', {email, password});
     }
     return(
         <div id="login">
@@ -12,11 +15,21 @@ const Login = () => {
             <form className="form" onSubmit={handleSubmit}>
                <div className="field">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" />
+                    <input 
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
                </div>
                <div className="field">
                     <label htmlFor="password">Senha</label>
-                    <input type="password" name="password" id="password" />
+                    <input 
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
                </div>
                <div className="actions">
                 <button type="submit">ENTRAR</button>
