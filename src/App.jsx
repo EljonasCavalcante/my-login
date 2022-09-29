6bb6d32b52ab19e30137166371c35011dd13f573
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-28 22:22:45 -0300

    update app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import AppRoutes from './routers/AppRoutes';
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <AppRoutes />        
+    </div>
+  );
+}
+
+export default App;
