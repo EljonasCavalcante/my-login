b47a8675c223c6bd8d8b68948660ff732df19821
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-10-11 14:33:03 -0300

    everything worked until now

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+
+import { AuthContext } from "../../contexts/auth";
+
 import './Login.scss';
 
-const Login = () => {
+const LoginPage = () => {
+    const {authenticated, login} = useContext(AuthContext);
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('submit', {email, password});
+
+        login(email,password);
+        
     }
     return(
         <div id="login">
-            <h1 className="title">Login do Sistema</h1>
+            <h1 className="title">Login do Sistema</h1>           
+                <p>{String(authenticated)}</p>
             <form className="form" onSubmit={handleSubmit}>
                <div className="field">
                     <label htmlFor="email">Email</label>
@@ -38,4 +47,4 @@ const Login = () => {
         </div>
     );
 };
-export default Login;
+export default LoginPage;
--- src/routers/AppRoutes.jsx
+++ src/routers/AppRoutes.jsx
@@ -1,4 +1,6 @@
 // yarn add react-router-dom
+
+import React from "react";
 import {
     BrowserRouter as Router,
     Route,
@@ -6,15 +8,18 @@ import {
     Navigate,
 } from "react-router-dom";
 import HomePage from "../pages/Home/Home";
-import Login from "../pages/Login/Login";
+import LoginPage from "../pages/Login/Login";
+import { AuthProvicer } from "../contexts/auth";
 
 const AppRoutes = () => {
     return(
         <Router>
-            <Routes>
-                <Route exact path="/login" element={<Login/>} />
-                <Route exact path="/login" element={<HomePage/>} />
-            </Routes>
+            <AuthProvicer>
+                <Routes>
+                    <Route exact path="/login" element={<LoginPage/>} />
+                    <Route exact path="/" element={<HomePage/>} />
+                </Routes>
+            </AuthProvicer>
         </Router>
     )
 };
